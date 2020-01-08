5f431404dbde37f15272ca50c2f64ab77106425c
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-01-08 12:20:53 -0600

    Filling out basic skeleton

--- foodiefunreact/src/components/RestaurantPage.js
+++ foodiefunreact/src/components/RestaurantPage.js
@@ -4,6 +4,20 @@ function RestaurantPage()
 {
     return (
         <div>
+            <h1>Restaurant Title</h1>
+            <h3>Cuisine: </h3>
+            <h3>Address: </h3>
+            <h3>City: </h3>
+            <h3>State: </h3>
+            <h3>Zip Code: </h3>
+
+            <div className='MenuCard'>
+                <h2>Restaurant Menu</h2>
+                <ul>
+                    <li>Menu Item</li>
+                    <p>Description of item</p>
+                </ul>
+            </div>
 
         </div>
     );
